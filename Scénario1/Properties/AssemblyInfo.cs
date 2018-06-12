731ab3d449fe339cee6aa8d52f5d285a781436f4
hafsa.jiat <Hafsa.Jiat_ext@s-money.fr>
2018-06-12 18:27:45 +0200

    msg

--- Scénario1/Properties/AssemblyInfo.cs
+++ Scénario1/Properties/AssemblyInfo.cs
@@ -1,10 +1,12 @@
-﻿using System.Reflection;
+﻿using NUnit.Framework;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // Les informations générales relatives à un assembly dépendent de
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
+//[assembly: Parallelizable(ParallelScope.Children)]
 [assembly: AssemblyTitle("Scénario1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
---
+++ Scénario1/RandomUtil.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scénario1
+{
+    public static class RandomUtil
+    {
+        private static Random random = new Random();
+
+        public static string GetRandomString()
+        {
+            string path = Path.GetRandomFileName();
+            path = path.Replace(".", "");
+            return path;
+        }
+
+        public static string RandomPhoneNumber()
+        {
+            const string chars = "0123456789";
+            return new string(Enumerable.Repeat(chars, 8)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
